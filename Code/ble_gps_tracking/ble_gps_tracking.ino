ee5b70eed81b6f8b1569c6b2845f716fb54ab100
Duncan <duncan@saduk.inc>
2024-11-10 22:57:53 -0500

    Sync of work on BLE GPS tracking

--- Code/ble_gps_tracking/ble_gps_tracking.ino
+++ Code/ble_gps_tracking/ble_gps_tracking.ino
@@ -81,6 +81,41 @@ void dumpInfo() {
   return;
 }
 
+// Function for Printing Collected GPS Information
+void gpsInfoDump() {
+  Serial.println(gps.location.lat(), 6); // Latitude in degrees (double)
+  Serial.println(gps.location.lng(), 6); // Longitude in degrees (double)
+  Serial.print(gps.location.rawLat().negative ? "-" : "+");
+  Serial.println(gps.location.rawLat().deg); // Raw latitude in whole degrees
+  Serial.println(gps.location.rawLat().billionths);// ... and billionths (u16/u32)
+  Serial.print(gps.location.rawLng().negative ? "-" : "+");
+  Serial.println(gps.location.rawLng().deg); // Raw longitude in whole degrees
+  Serial.println(gps.location.rawLng().billionths);// ... and billionths (u16/u32)
+  Serial.println(gps.date.value()); // Raw date in DDMMYY format (u32)
+  Serial.println(gps.date.year()); // Year (2000+) (u16)
+  Serial.println(gps.date.month()); // Month (1-12) (u8)
+  Serial.println(gps.date.day()); // Day (1-31) (u8)
+  Serial.println(gps.time.value()); // Raw time in HHMMSSCC format (u32)
+  Serial.println(gps.time.hour()); // Hour (0-23) (u8)
+  Serial.println(gps.time.minute()); // Minute (0-59) (u8)
+  Serial.println(gps.time.second()); // Second (0-59) (u8)
+  Serial.println(gps.time.centisecond()); // 100ths of a second (0-99) (u8)
+  Serial.println(gps.speed.value()); // Raw speed in 100ths of a knot (i32)
+  Serial.println(gps.speed.knots()); // Speed in knots (double)
+  Serial.println(gps.speed.mph()); // Speed in miles per hour (double)
+  Serial.println(gps.speed.mps()); // Speed in meters per second (double)
+  Serial.println(gps.speed.kmph()); // Speed in kilometers per hour (double)
+  Serial.println(gps.course.value()); // Raw course in 100ths of a degree (i32)
+  Serial.println(gps.course.deg()); // Course in degrees (double)
+  Serial.println(gps.altitude.value()); // Raw altitude in centimeters (i32)
+  Serial.println(gps.altitude.meters()); // Altitude in meters (double)
+  Serial.println(gps.altitude.miles()); // Altitude in miles (double)
+  Serial.println(gps.altitude.kilometers()); // Altitude in kilometers (double)
+  Serial.println(gps.altitude.feet()); // Altitude in feet (double)
+  Serial.println(gps.satellites.value()); // Number of satellites in use (u32)
+  Serial.println(gps.hdop.value()); // Horizontal Dim. of Precision (100ths-i32)
+}
+
 // Function to Trim White Space
 void trim(char *str) {
   int i;
@@ -206,10 +241,23 @@ void loop() {
   sqlite3_stmt *stmt;
   int count = 0;
 
+
+  // GPS Interaction
   while (SerialPort.available() > 0) {
     if (gps.encode(SerialPort.read())) {
+      Serial.println("[*] Dumping GPS Info");
       dumpInfo();
+      Serial.println("[*] Printing GPS Info");
+      gpsInfoDump();
     }
+  }
+  // No GPS Device Detected
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+    Serial.println(F("No GPS detected: check wiring."));
+  }
+  // No GPS Data
+  if (gps.charsProcessed() < 10) {
+    Serial.println(F("No GPS data received: check wiring"));
   }
 
   // BLE Device Scanning
@@ -217,10 +265,12 @@ void loop() {
   BLEAdvertisedDevice currentDevice;
   // Parse the Data from the Found Devices
   n = foundDevices->getCount();
+  Serial.printf("[*] Devices Found: %i \n", n);
   for (int i = 0; i < n;  ++i) {
     currentDevice = foundDevices->getDevice(i);
 
     if ((gps.location.isValid()) && (year > 2023)) {
+      Serial.printf("[+] Logging Attempt");
       // Gather Data
       name = currentDevice.getName().c_str();
       if (sqlite3_prepare_v2(db1, "SELECT * FROM ble_data WHERE MAC=?", -1, &stmt, NULL) == SQLITE_OK) {
@@ -295,7 +345,7 @@ void loop() {
   char my_count[40] = { 0 };
 
 #ifdef OLED
-  if(oled_mode == 5)
+  if(oled_mode == 6)
     oled_mode = 0;
 
   switch(oled_mode)
@@ -330,7 +380,7 @@ void loop() {
         }
       }
       sqlite3_finalize(stmt);
-      snprintf(my_count, sizeof(my_count) - 1, "%d", count);
+      snprintf(my_count, sizeof(my_count) - 1, "Logged Devs: %d", count);
       display.clear();
       display.drawString(0, OLED_OFFSET, my_count);
       display.display();
@@ -344,7 +394,7 @@ void loop() {
         }
       }
       sqlite3_finalize(stmt);
-      snprintf(my_count, sizeof(my_count) - 1, "HID?: %d", count);
+      snprintf(my_count, sizeof(my_count) - 1, "HIDs: %d", count);
       display.clear();
       display.drawString(0, OLED_OFFSET, my_count);
       display.display();
@@ -363,6 +413,12 @@ void loop() {
         display.drawString(0, OLED_OFFSET, " NO GPS TIME");
         display.display();
       }
+      break;
+    case 5: // Display Satellites in Use
+      snprintf(my_count, sizeof(my_count) - 1, "Sats: %d", gps.satellites.value());
+      display.clear();
+      display.drawString(0, OLED_OFFSET, my_count);
+      display.display();
       break;
   }
 
